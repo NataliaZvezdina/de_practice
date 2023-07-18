b1a89f4ab619018872c3d03498659fc9747b4a28
NataliaZvezdina <natalia.2vezd1na@yandex.ru>
2023-07-18 17:13:18 +0300

    reformat code

--- mapreduce/reducer.py
+++ mapreduce/reducer.py
@@ -2,10 +2,6 @@
 
 import sys
 
-current_key = None
-total_tips = 0
-count = 0
-
 mp = {'1': 'Credit card',
       '2': 'Cash',
       '3': 'No charge',
@@ -13,23 +9,33 @@ mp = {'1': 'Credit card',
       '5': 'Unknown',
       '6': 'Voided trip'}
 
-for line in sys.stdin:
-    key, value = line.split('\t')
 
-    if key != current_key:
-        if current_key:
-            avg_tip = total_tips / count
-            payment_type, month = current_key.split(',')
-            print('{0},{1},{2:.2f}'.format(mp[payment_type], month, avg_tip))
-        current_key = key
+def perform_reduce():
+    current_key = None
+    total_tips = 0
+    count = 0
 
-        total_tips = 0
-        count = 0
+    for line in sys.stdin:
+        key, value = line.split('\t')
 
-    total_tips += float(value)
-    count += 1
+        if key != current_key:
+            if current_key:
+                avg_tip = total_tips / count
+                payment_type, month = current_key.split(',')
+                print('{0},{1},{2:.2f}'.format(mp[payment_type], month, avg_tip))
+            current_key = key
 
-if current_key:
-    avg_tip = total_tips / count
-    payment_type, month = current_key.split(',')
-    print('{0},{1},{2:.2f}'.format(mp[payment_type], month, avg_tip))
+            total_tips = 0
+            count = 0
+
+        total_tips += float(value)
+        count += 1
+
+    if current_key:
+        avg_tip = total_tips / count
+        payment_type, month = current_key.split(',')
+        print('{0},{1},{2:.2f}'.format(mp[payment_type], month, avg_tip))
+
+
+if __name__ == '__main__':
+    perform_reduce()
